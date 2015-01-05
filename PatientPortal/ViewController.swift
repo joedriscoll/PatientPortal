6246b1b3495db1bef59408f45fee934c475f3988
joedriscoll <josephdr@mit.edu>
2015-01-05 15:53:40 -0800

    first commit

---
+++ PatientPortal/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PatientPortal
+//
+//  Created by Joseph Driscoll on 1/5/15.
+//  Copyright (c) 2015 Joseph Driscoll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else{
+            
+            self.usernameLabel.text = prefs.valueForKey("USERNAME") as NSString
+        }
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func logoutTapped(sender: UIButton) {
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+
+}
+
