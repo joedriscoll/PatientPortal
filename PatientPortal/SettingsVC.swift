46040af46cd1db39461eb263e62bdfe4ade65e62
joedriscoll <josephdr@mit.edu>
2015-01-08 13:11:59 -0800

    added some stuff for settings

---
+++ PatientPortal/SettingsVC.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsVC.swift
+//  PatientPortal
+//
+//  Created by Joseph Driscoll on 1/8/15.
+//  Copyright (c) 2015 Joseph Driscoll. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    
+    @IBOutlet weak var changePTButton: UIButton!
+    
+    @IBOutlet weak var assingedPTLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
