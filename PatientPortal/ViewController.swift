663d761e106e4c975c7447131d457fc5f689fa78
joedriscoll <josephdr@mit.edu>
2015-01-08 19:07:48 -0800

    added logPain

--- PatientPortal/ViewController.swift
+++ PatientPortal/ViewController.swift
@@ -51,6 +51,85 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func logPainTapped(sender: UIButton) {
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let session_key = prefs.valueForKey("SESSION_KEY") as NSString
+        
+        var post:NSString = "session_key=\(session_key)&data=None"
+        
+        NSLog("PostData: %@",post);
+        
+        var url:NSURL = NSURL(string: "http://localhost:8000/ptapi/logPain")!
+        
+        var postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+        
+        var postLength:NSString = String( postData.length )
+        
+        var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.HTTPBody = postData
+        request.setValue(postLength, forHTTPHeaderField: "Content-Length")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        
+        var reponseError: NSError?
+        var response: NSURLResponse?
+        
+        var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&reponseError)
+        
+        if ( urlData != nil ) {
+            let res = response as NSHTTPURLResponse!;
+            
+            NSLog("Response code: %ld", res.statusCode);
+            
+            if (res.statusCode >= 200 && res.statusCode < 300)
+            {
+                var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                
+                NSLog("Response ==> %@", responseData);
+                
+                var error: NSError?
+                
+                let jsonData:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &error) as NSDictionary
+                
+                
+                let success:NSInteger = jsonData.valueForKey("success") as NSInteger
+                
+                //[jsonData[@"success"] integerValue];
+                
+                NSLog("Success: %ld", success);
+                
+                if(success == 1)
+                {
+                    var alertView:UIAlertView = UIAlertView()
+                    alertView.title = "Pain Logged"
+                    alertView.message = "Your Pain was Logged"
+                    alertView.delegate = self
+                    alertView.addButtonWithTitle("OK")
+                    alertView.show()
+                    
+                } else {
+                    var error_msg:NSString
+                    
+                    if jsonData["error_message"] as? NSString != nil {
+                        error_msg = jsonData["error_message"] as NSString
+                    } else {
+                        error_msg = "Unknown Error"
+                    }
+                    var alertView:UIAlertView = UIAlertView()
+                    alertView.title = "A Problem Occured!"
+                    alertView.message = error_msg
+                    alertView.delegate = self
+                    alertView.addButtonWithTitle("OK")
+                    alertView.show()
+                    
+                }
+                
+            }
+        }
+
+    }
     @IBAction func logoutTapped(sender: UIButton) {
         let appDomain = NSBundle.mainBundle().bundleIdentifier
         NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
